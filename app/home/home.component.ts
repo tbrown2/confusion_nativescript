12243f9b3f6275e5abb9a0c8a6245b181f60f585
Tom <thomasmbrown11@gmail.com>
2017-12-03 19:54:36 -0800

    added some minor fixes

--- app/home/home.component.ts
+++ app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Inject, ChangeDetectorRef } from '@angular/core';
-import {Dish} from '../shared/dish';
+
+import { Dish } from '../shared/dish';
 import { DishService } from '../services/dish.service';
 import { Promotion } from '../shared/promotion';
 import { PromotionService } from '../services/promotion.service';
@@ -13,26 +14,24 @@ import { DrawerPage } from '../shared/drawer/drawer.page';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-
 export class HomeComponent extends DrawerPage implements OnInit {
-	dish: Dish;
-	promotion: Promotion;
-	leader: Leader;
-	dishErrMess: string;
-	promotErrMess: string;
-	leaderErrMess: string;
 
-	constructor (private dishservice: DishService,
-		private promotionservice: PromotionService, 
-		private  leaderservice: LeaderService,
-		private changeDetectorRef: ChangeDetectorRef,  
-		@Inject('BaseURL') private BaseURL)
-		) {
-			//invokes the constructor of the extended class
-			super(changeDetectorRef);
-		}
+  dish: Dish;
+  promotion: Promotion;
+  leader: Leader;
+  dishErrMess: string;
+  promoErrMess: string;
+  leaderErrMess: string;
 
-	ngOnInit() {
+  constructor(private dishservice: DishService,
+    private promotionservice: PromotionService,
+    private leaderservice: LeaderService,
+    private changeDetectorRef:ChangeDetectorRef,
+    @Inject('BaseURL') private BaseURL) {
+      super(changeDetectorRef);
+    }
+
+  ngOnInit() {
     this.dishservice.getFeaturedDish()
        .subscribe(dish => this.dish = dish,
         errmess => this.dishErrMess = <any>errmess );
@@ -43,4 +42,5 @@ export class HomeComponent extends DrawerPage implements OnInit {
       .subscribe(leader => this.leader = leader,
         errmess => this.leaderErrMess = <any>errmess );
   }
+
 }
--- app/shared/promotion.ts
+++ app/shared/promotion.ts
@@ -1,11 +1,9 @@
 export interface Promotion {
-	id: number;
-	name: string;
-	name: string;
-	label: string;
-	price: string;
-	featured: boolean;
-	description: string;
-
-	
+    id: number;
+    name: string;
+    image: string;
+    label: string;
+    price: string;
+    featured: boolean;
+    description: string;
 }
