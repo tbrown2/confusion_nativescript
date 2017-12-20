d291093ec5234f5b1ef72b8d11adbcd4df5a6f0d
Tom <thomasmbrown11@gmail.com>
2017-12-19 16:52:36 -0800

    added foundations for login

--- app/shared/drawer/drawer.component.ts
+++ app/shared/drawer/drawer.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { TNSFontIconService } from 'nativescript-ngx-fonticon';
+import {login, LoginResult } from 'ui/dialogs';
+import { getString, setString } from 'application-settings';
+
+
 @Component({
 	selector: 'drawer-content',
 	templateUrl: './shared/drawer/drawer.component.html',
@@ -8,4 +12,24 @@ import { TNSFontIconService } from 'nativescript-ngx-fonticon';
 
 export class DrawerComponent {
 	constructor (private fonticon: TNSFontIconService) {}
+
+	displayLoginDialog() {
+		//should encrypt password
+		let options = {
+			title: "Login",
+			message: "Type your login credentials",
+			userName: getString("userName", ""),
+			password: getString("password",""),
+			okButtonText: "Login",
+			cancelButton: "Cancel"
+		}
+
+		login(options)
+            .then((loginResult: LoginResult) => {
+                setString("userName", loginResult.userName);
+                setString("password", loginResult.password);
+            },
+            () => { console.log('Login cancelled'); 
+        });
+	}
 }
