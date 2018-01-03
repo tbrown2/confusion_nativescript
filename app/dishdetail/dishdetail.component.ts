f5b51528c73a0112ae0a5ef276c1c6493a0bec8a
Tom <thomasmbrown11@gmail.com>
2018-01-02 17:05:41 -0800

    reconfigured android manifest file

--- app/dishdetail/dishdetail.component.ts
+++ app/dishdetail/dishdetail.component.ts
@@ -8,6 +8,15 @@ import 'rxjs/add/operator/switchMap';
 import { FavoriteService } from '../services/favorite.service';
 import { TNSFontIconService } from 'nativescript-ngx-fonticon';
 import { Toasty } from 'nativescript-toasty';
+
+//animation imports 
+import { Page } from "ui/page";
+import {Animation, AnimationDefinition } from "ui/animation";
+import { View } from "ui/core/view";
+import { SwipeDirection, SwipeGestureEventData } from "ui/gestures";
+import { Color } from "color";
+import * as enums from "ui/enums";
+
 @Component ({
 	selector: 'app-dishdetail', 
 	moduleId: module.id,
@@ -24,12 +33,20 @@ export class DishdetailComponent implements OnInit {
 	numcomments: number;
 	favorite: boolean = false;
 
+	//animation related variables
+	showComments: boolean = false;
+
+	cardImage: View;
+	commentList: View;
+	cardLayout: View;
+
 	constructor(private dishservice: DishService,
 		@Inject('BaseURL') private BaseURL,
 		private route: ActivatedRoute,
 		private routerExtensions: RouterExtensions,
 		private favoriteservice: FavoriteService,
-		private fonticon: TNSFontIconService) { }
+		private fonticon: TNSFontIconService,
+		private page: Page) { }
 
 	ngOnInit() {
 		this.route.params
@@ -60,4 +77,98 @@ export class DishdetailComponent implements OnInit {
 	goBack() {
 		this.routerExtensions.back();
 	}
+
+	onSwipe(args: SwipeGestureEventData) {
+
+    if (this.dish) {
+      this.cardImage = <View>this.page.getViewById<View>("cardImage");
+      this.cardLayout = <View>this.page.getViewById<View>("cardLayout");
+      this.commentList = <View>this.page.getViewById<View>("commentList");
+
+      if (args.direction === SwipeDirection.up && !this.showComments ) {
+        this.animateUp();
+      }
+      else if (args.direction === SwipeDirection.down && this.showComments ) {
+        this.showComments = false;
+        this.animateDown();
+      }
+    }
+
+  }
+
+	showAndHideComments() {
+      this.cardImage = <View>this.page.getViewById<View>("cardImage");
+      this.cardLayout = <View>this.page.getViewById<View>("cardLayout");
+      this.commentList = <View>this.page.getViewById<View>("commentList");
+
+      if (!this.showComments ) {
+        this.animateUp();
+      }
+      else if (this.showComments ) {
+        this.showComments = false;
+        this.animateDown();
+      }
+	}
+
+	animateUp() {
+		let definitions = new Array<AnimationDefinition>();
+	    let a1: AnimationDefinition = {
+	        target: this.cardImage,
+	        scale: { x: 1, y: 0 },
+	        translate: { x: 0, y: -200 },
+	        opacity: 0,
+	        duration: 500,
+	        curve: enums.AnimationCurve.easeIn
+	    };
+	    definitions.push(a1);
+
+	    let a2: AnimationDefinition = {
+	        target: this.cardLayout,
+	        backgroundColor: new Color("#ffc107"),
+	        duration: 500,
+	        curve: enums.AnimationCurve.easeIn
+	    };
+	    definitions.push(a2);
+
+	    let animationSet = new Animation(definitions);
+
+	    animationSet.play().then(() => {
+	      this.showComments = true;
+	    })
+	    .catch((e) => {
+	        console.log(e.message);
+	    });
+
+	}
+
+	animateDown(){
+		let definitions = new Array<AnimationDefinition>();
+	    let a1: AnimationDefinition = {
+	        target: this.cardImage,
+	        scale: { x: 1, y: 1 },
+	        translate: { x: 0, y: 0 },
+	        opacity: 1,
+	        duration: 500,
+	        curve: enums.AnimationCurve.easeIn
+	    };
+	    definitions.push(a1);
+
+	    let a2: AnimationDefinition = {
+	        target: this.cardLayout,
+	        backgroundColor: new Color("#ffffff"),
+	        duration: 500,
+	        curve: enums.AnimationCurve.easeIn
+	    };
+	    definitions.push(a2);
+
+	    let animationSet = new Animation(definitions);
+
+	    animationSet.play().then(() => {
+	    })
+	    .catch((e) => {
+	        console.log(e.message);
+	    });
+	}
+
+	
 }
